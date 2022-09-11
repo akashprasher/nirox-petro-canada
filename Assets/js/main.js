554eea5f7b9df1a608add8e06fc609c6182bc9ab
Akash Prasher <prince.akash.prasher@gmail.com>
2022-09-11 06:33:03 +0530

    Adding pop-up function and styling respectively

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,1 +1,14 @@
 // JavaScript File.
+let popupClass = document.getElementById("pop-up");
+
+let popup = () => {
+  setTimeout(() => {
+    popupClass.style.display = "flex";
+  }, 2000);
+};
+
+let crossButton = document.getElementById("cross");
+
+crossButton.addEventListener("click", () => {
+  popupClass.style.display = "none";
+});
